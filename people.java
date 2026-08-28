5ac3d7e42a4f32c3fb9b29fb4e84c4d39e820bb0
Varshinie Machha <vm2482@srmist.edu.in>
2026-08-28 19:13:46 +0530

    initial commit of people

---
+++ people.java
@@ -0,0 +1,23 @@
+class Employee{
+    String empName;
+    double salary;
+    static String companyName = "Bright Horizon Technologies";
+    static int employeeCount = 0;
+    Employee(String empName, double salary) {
+        this.empName = empName;
+        this.salary = salary;
+        employeeCount++;
+    }
+    static void printCompanyInfo(){
+        System.out.println(companyName);
+        System.out.println("Employees on record: " + employeeCount);
+    }
+}
+public class people{
+    public static void main(String[] args){
+        Employee e1 = new Employee("Priya", 50000);
+        Employee e2 = new Employee("Arjun", 55000);
+        Employee e3 = new Employee("Divya", 60000);
+        Employee.printCompanyInfo();
+    }
+}
